9b0d739a93244e436a436c0f5c943127835bab3e
Vaggelis Agapiou <vaggap73@gmail.com>
2026-05-05 22:12:35 +0300

    Split logic into different components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
-/*eslint-disable no-unused-vars */
-import { useState } from "react";
-import { callHistory } from "./data/calls";
+import Home from "./components/Home";
 
 function App() {
-  const [calls, setCalls] = useState(callHistory);
-
   return (
     <>
-      {calls.map((call) => (
-        <span key={call.id}>
-          {call.direction} call from {call.from} to {call.to}
-          <p>{call.call_type}</p>
-        </span>
-      ))}
+      <Home />
     </>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,20 @@
+/*eslint-disable no-unused-vars */
+import { useState } from "react";
+import { callHistory } from "../data/calls";
+
+function Home() {
+  const [calls, setCalls] = useState(callHistory);
+
+  return (
+    <div>
+      {calls.map((call) => (
+        <span key={call.id}>
+          {call.direction} call from {call.from} to {call.to}
+          <p>{call.call_type}</p>
+        </span>
+      ))}
+    </div>
+  );
+}
+
+export default Home;
