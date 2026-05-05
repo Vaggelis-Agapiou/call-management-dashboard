a59daaa9dd6a9a1f05197b6d3feae456cdfb1fe8
Vaggelis Agapiou <vaggap73@gmail.com>
2026-05-05 21:53:17 +0300

    Confirming data is being rendered correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+/*eslint-disable no-unused-vars */
+import { useState } from "react";
+import { callHistory } from "./data/calls";
+
 function App() {
-  return <></>;
+  const [calls, setCalls] = useState(callHistory);
+
+  return (
+    <>
+      {calls.map((call) => (
+        <span key={call.id}>
+          {call.direction} call from {call.from} to {call.to}
+          <p>{call.call_type}</p>
+        </span>
+      ))}
+    </>
+  );
 }
 
 export default App;
