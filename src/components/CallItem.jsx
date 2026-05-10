461322e4fe23ee7aa54301249b013678a106c7ca
Vaggelis Agapiou <vaggap73@gmail.com>
2026-05-10 11:36:12 +0300

    Added styling to Home & CallItem components

--- src/components/CallItem.jsx
+++ src/components/CallItem.jsx
@@ -4,12 +4,31 @@ function CallItem({ call }) {
   const navigate = useNavigate();
 
   return (
-    <div key={call.id} onClick={() => navigate(`/call/${call.id}`)}>
+    <div
+      key={call.id}
+      onClick={() => navigate(`/call/${call.id}`)}
+      className=" bg-white rounded-xl border-2 border-gray-400 cursor-pointer hover:shadow-md p-5 my-3 flex justify-between items-center capitalize"
+    >
+      <div className="flex flex-col gap-2 text-center">
+        <p
+          className={`${call.direction === "inbound" ? "text-blue-600" : "text-purple-500"} text-s font-medium`}
+        >
+          {call.direction}
+        </p>
+        <p className="rounded bg-amber-300 px-1 font-medium text-s">
+          {call.call_type}
+        </p>
+      </div>
       <p>
-        {call.direction} call from {call.from} to {call.to}
-        <br />
-        <span>{call.call_type}</span>
+        {call.from} ➡️ {call.to}
       </p>
+      <div className="flex flex-col">
+        <p className="text-s text-gray-600">{call.created_at}</p>
+        <p className="text-s text-gray-600">{call.duration} min</p>
+      </div>
+      <button className="border rounded-xl p-1 hover:cursor-pointer hover:bg-slate-800 hover:text-slate-50">
+        Archive
+      </button>
     </div>
   );
 }
--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -6,20 +6,30 @@ function Details() {
   const call = callHistory.find((c) => c.id.toString() === id);
 
   return (
-    <div>
-      <h2>Call #{call.id} Details</h2>
-      <p>Direction: {call.direction}</p>
-      <p>From: {call.from}</p>
-      <p>To: {call.to}</p>
-      <p>Type: {call.call_type}</p>
-      <p>Duration: {call.duration}</p>
-      <p>Date: {call.created_at}</p>
-      <p>Archived: {call.is_archived === true ? "Yes" : "No"}</p>
-      {call.notes ? (
-        call.notes.map((note) => <p key={note.id}>{note.content}</p>)
-      ) : (
-        <p>No notes for this call.</p>
-      )}
+    <div className="min-h-screen bg-gray-200">
+      <div className="mb-10 py-8 bg-slate-900 outline-2 shadow-md shadow-slate-400">
+        <h1 className="pl-4 text-3xl font-bold text-blue-50">
+          DevReady Call Center
+        </h1>
+      </div>
+      <div>
+        <h1>DevReady Call Center</h1>
+      </div>
+      <div>
+        <h2>Call #{call.id} Details</h2>
+        <p>Direction: {call.direction}</p>
+        <p>From: {call.from}</p>
+        <p>To: {call.to}</p>
+        <p>Type: {call.call_type}</p>
+        <p>Duration: {call.duration}</p>
+        <p>Date: {call.created_at}</p>
+        <p>Archived: {call.is_archived === true ? "Yes" : "No"}</p>
+        {call.notes ? (
+          call.notes.map((note) => <p key={note.id}>{note.content}</p>)
+        ) : (
+          <p>No notes for this call.</p>
+        )}
+      </div>
     </div>
   );
 }
