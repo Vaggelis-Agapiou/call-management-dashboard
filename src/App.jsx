27f9b0aa16b1cb3c312f3482328d0db4c41cc23b
Vaggelis Agapiou <vaggap73@gmail.com>
2026-05-16 00:07:36 +0300

    Established connection with API & fetching of all calls

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import { useState } from "react";
-import { callHistory } from "./data/calls";
+import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Details from "./pages/Details";
+import { getCalls } from "./services/api";
 
 function App() {
-  const [calls, setCalls] = useState(callHistory);
+  const [calls, setCalls] = useState([]);
+  //eslint-disable-next-line no-unused-vars
+  const [loading, setLoading] = useState(true);
+  //eslint-disable-next-line no-unused-vars
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    getCalls()
+      .then((data) => setCalls(data.calls))
+      .catch((error) => setError(error.message))
+      .finally(() => setLoading(false));
+  }, []);
+
+  console.log(calls);
 
   function handleArchive(id) {
     setCalls((allCalls) =>
--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,15 @@
-const BASE_URL = "https://call-center-mu.vercel.app/";
+const BASE_URL = "https://call-center-mu.vercel.app";
 
 const headers = {
   "X-User-Id": "vaggelis-agapiou",
 };
+
+export async function getCalls() {
+  const response = await fetch(`${BASE_URL}/calls`, { headers });
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch Calls");
+  }
+
+  return response.json();
+}
